f9d2af98b0c077aaa9916b38ad775accc37f3e9e
Akeem Allen <allenakeem8@gmail.com>
2020-07-22 19:54:53 -0500

    Added scss support and routing with react router dom

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../stylesheets/Landing.scss";
+
+const Landing = () => {
+  return (
+    <div className="main">
+      <h1 className="header">Landing Page</h1>
+    </div>
+  );
+};
+
+export default Landing;
