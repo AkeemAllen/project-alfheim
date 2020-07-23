97aa3109787082afc10e098426f8011de5a14f0c
Akeem Allen <allenakeem8@gmail.com>
2020-07-22 20:44:24 -0500

    Added Primary Colors, Fonts and Defaults to Css

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import React from "react";
-import "./App.css";
+import "./main.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import routes from "./router";
 
-class App extends React.Component {
-  render() {
-    return (
-      <Router basename="">
-        <div>
-          <Switch>
-            {routes.map((route, index) => {
-              return (
-                <Route
-                  key={index}
-                  path={route.path}
-                  exact={route.exact}
-                  component={(props) => <route.component {...props} />}
-                />
-              );
-            })}
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Router basename="">
+      <div>
+        <Switch>
+          {routes.map((route, index) => {
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                exact={route.exact}
+                component={(props) => <route.component {...props} />}
+              />
+            );
+          })}
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
