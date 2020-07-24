3b89de1a17d80074cd1a1ce483c3a73dbacbb981
Akeem Allen <allenakeem8@gmail.com>
2020-07-24 11:19:16 -0500

    Sign Up Ui Complete

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,20 +1,40 @@
 import React from "react";
 import "../stylesheets/SignUp.scss";
+import { MdAccountCircle, MdLock } from "react-icons/md";
+import Footer from "../components/Footer";
 
 const SignUp = () => {
   return (
-    <div className="signup-container">
-      <div className="form">
-        <div className="form-logo">
-          <h3>Alfheim</h3>
+    <div>
+      <div className="signup-container">
+        <div className="nav">
+          <button className="btn">Home</button>
+          <button className="btn">Gallary</button>
         </div>
-        <h3 className="message">
-          <strong>Sign In</strong> to view you account
-        </h3>
-        <div className="form-content">
-          <input className="form-input"></input>
+        <div className="form">
+          <div className="form-logo">
+            <h3>Alfheim</h3>
+          </div>
+          <h3 className="message">
+            <strong>Sign In</strong> to view you account
+          </h3>
+          <div className="form-content">
+            <div className="input">
+              <MdAccountCircle size="30" className="input-icon" />
+              <input
+                className="form-input"
+                placeholder="Username/Email"
+              ></input>
+            </div>
+            <div className="input">
+              <MdLock size="30" className="input-icon" />
+              <input className="form-input" placeholder="Password"></input>
+            </div>
+            <button>Sign In</button>
+          </div>
         </div>
       </div>
+      {/* <Footer /> */}
     </div>
   );
 };
