d87bf24d42cf863a02050dd07355494a6ed6a6e8
Akeem Allen <allenakeem8@gmail.com>
2020-09-08 16:59:26 -0500

    Issues with Update Button

--- src/components/Modals/UpdateRoomModal.jsx
+++ src/components/Modals/UpdateRoomModal.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Modal from "../Modal";
 import { BoxedInput } from "../Inputs";
 import { NormalButton } from "../Buttons";
@@ -12,14 +12,42 @@ const UpdateRoomModal = ({
   data,
   fieldValue,
 }) => {
+  const [disabled, setDisabled] = useState(true);
+
+  useEffect(() => {
+    if ((field === "isAvailable") | (field === "isVisible")) {
+      setDisabled(false);
+    } else if (
+      (fieldValue === "") |
+      (fieldValue === undefined) |
+      (fieldValue === null) |
+      (fieldValue === 0)
+    ) {
+      setDisabled(true);
+    } else {
+      setDisabled(false);
+    }
+  }, [fieldValue, field]);
+
+  const closeModal = () => {
+    if (field === "price") {
+      setFieldValue(0);
+    } else {
+      setFieldValue("");
+    }
+    closeHandler();
+  };
+
   return (
-    <Modal open={open} handleClose={closeHandler}>
+    <Modal open={open} handleClose={closeModal}>
       <div style={{ display: "grid", rowGap: "1rem" }}>
         {(field === "isVisible") | (field === "isAvailable") ? null : (
           <BoxedInput
             type={field === "price" ? "number" : "text"}
             label={field}
-            onChange={(e) => setFieldValue(e.target.value)}
+            onChange={(e) => {
+              setFieldValue(e.target.value);
+            }}
           />
         )}
         <NormalButton
@@ -34,12 +62,7 @@ const UpdateRoomModal = ({
               ? "Add New Amenity"
               : `Update ${field}`
           }
-          disabled={
-            (fieldValue === null) |
-            (fieldValue === undefined) |
-            (fieldValue === "") |
-            (fieldValue === 0)
-          }
+          disabled={disabled}
           onClick={handleUpdate}
         />
       </div>
